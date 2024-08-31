c6080e6d1c544292a26269cfaafdfc31aef8eb3e
Pedro Buffon <henriquebuffon@gmail.com>
2024-08-30 22:23:21 -0300

    update script

--- install.sh
+++ install.sh
@@ -6,21 +6,25 @@ echo "THIS SCRIPT IS STILL IN DEVELOPMENT. DON'T USE IT IF YOU DON'T KNOW WHAT Y
 command_exists() {
     command -v "$1" >/dev/null 2>&1
 }
+
 # Function that detects which OS is running
 check_system_os(){
-    grep -E '^NAME=' /etc/os-release | sed 's/^NAME=//g' | tr -d '"' >/dev/null 2>&1
+    grep -E '^NAME=' /etc/os-release | sed 's/^NAME=//g' | tr -d '"'
 }
 
 dependencies="zsh fzf stow eza btop git yazi"
 
+# Check the OS and print it for debugging
+check_os=$(check_system_os)
+
 # Check which package manager to use based on the distribution
-if command_exists dnf && check_system_os === Fedora; then
+if command_exists dnf && [ "$check_os" = "Fedora" ]; then
     echo "Detected Fedora or CentOS/RHEL"
     dnf install $dependencies
-elif command_exists apt-get && { [ "$check_system_os" = "Ubuntu" ] || [ "$check_system_os" = "Debian" ] || [ "$check_system_os" = "Debian GNU/Linux" ]; }; then
+elif command_exists apt-get && { [ "$check_os" = "Ubuntu" ] || [ "$check_os" = "Debian" ] || [ "$check_os" = "Debian GNU/Linux" ]; }; then
     echo "Detected Ubuntu or Debian"
     apt-get update && apt-get install $dependencies
-elif command_exists yay && check_system_os === Arch Linux; then
+elif command_exists yay && [ "$check_os" = "Arch Linux"]; then
     echo "Detected Arch"
     yay -Syu
 else
@@ -30,7 +34,7 @@ fi
 
 # Clone the repository
 echo "Cloning the repository..."
-git clone https://github.com/PedroBuffon/dotfiles.git "~/dotfiles"
+git clone https://github.com/PedroBuffon/dotfiles.git ~/dotfiles
 
 # Display the ports to the user
 echo "Setup completed. Please log out and log in"
